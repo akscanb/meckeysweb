905b353e5058989d9a47698920e1ebbc7d8a2879
Andrew Koh <akscanb@gmail.com>
2020-03-21 22:49:38 -0700

    added shit

--- src/webcrawler.py
+++ src/webcrawler.py
@@ -1,13 +1,16 @@
 import praw
 import os
+import time
 
 reddit = praw.Reddit(
-    client_id=print(os.environ['CLIENT_ID']), 
-    client_secret=print(os.environ['CLIENT_SECRET']),
-    user_agent=print(os.environ['USER_AGENT'])
+    client_id=os.environ['CLIENT_ID'], 
+    client_secret=os.environ['CLIENT_SECRET'],
+    user_agent=os.environ['USER_AGENT']
 )
 
 print(reddit.read_only)
-
-for submission in reddit.subreddit('mechmarket').hot(limit=10):
+current_timestamp = time.time()
+two_months_timestamp = current_timestamp - (60*60*24*60)
+for submission in reddit.subreddit('mechmarket').stream.submissions():
     print(submission.title)
+    print(submission.link_flair_text)
